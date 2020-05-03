87ce12753e4a143ead17664314a06d01be8403fa
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-03 18:08:45 +0200

    feat: create Table component and Home page component.

---
+++ src/homePage/HomePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const HomePage = () => {
+
+    return(
+        <p>hello home</p>
+    );
+}
---
+++ src/table/Table.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Table = () => {
+
+    return(<p>Hello table</p>);
+}
