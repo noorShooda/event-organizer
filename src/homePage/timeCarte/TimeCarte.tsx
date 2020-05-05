7757994b41c3b38330f56258507672187eb056b6
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-05 18:41:41 +0200

    feat: create TimeCarte component to handle the time display and add the suitble CSS style

---
+++ src/homePage/timeCarte/TimeCarte.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './timeCarte.css';
+
+interface Props {
+    time: number;
+}
+
+export const TimeCarte: React.FunctionComponent<Props>= (props) => {
+
+    return(
+        <div className="timeCarteStyle">
+            <p>{props.time}</p>
+        </div>
+    );
+}
