d052a342263c5c897c3419bf76246eaf9ab7e918
noorShooda <sebai.noorelhooda@gmail.com>
2020-05-05 18:39:21 +0200

    refactor: convert the HomePage component from javascript to typescript.

---
+++ src/homePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { TimeCarte } from './timeCarte/TimeCarte';
+import { showTodayTime } from './timeCarte/helper';
+
+export const HomePage: React.FunctionComponent = () => {
+
+    
+    return(
+        <div>
+            <TimeCarte time={showTodayTime()}/>
+        </div>
+    );
+}
